e3c14ee5cd13304571cb027e50ae3dcaa198b056
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-07-24 13:35:16 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
-#!/bin/sh -l
+#!/bin/bash
+set -e
 
 if [ "x$INPUT_INSTALL" != "x" ]; then
+    echo "Installing polyfill-glibc to $GITHUB_PATH"
     echo $(find $(pwd) -name 'polyfill-glibc' -printf "%h\n" | sort -u) >> $GITHUB_PATH
     echo "::set-output name=binary::$(find $(pwd) -name 'polyfill-glibc' -printf "%h\n" | sort -u)/polyfill-glibc"
 fi
 
 if [ "x$INPUT_TARGET" != "x" ] && [ "x$INPUT_GLIBC" != "x" ]; then
     if [ "x$INPUT_TARGET" != "x" ]; then
+         echo "Running polyfill-glibc with target $INPUT_TARGET"
         cp $INPUT_TARGET /tmp/$INPUT_TARGET
         name=$(sudo sh -c "polyfill-glibc --target-glibc=$INPUT_GLIBC $INPUT_TARGET || echo $INPUT_TARGET")
     fi
@@ -14,6 +17,6 @@ if [ "x$INPUT_TARGET" != "x" ] && [ "x$INPUT_GLIBC" != "x" ]; then
         exit 1
     fi
     echo "::set-output name=binary::$name"
-#else
-#    sh -c "$@"
+else
+    sh -c "$@"
 fi
