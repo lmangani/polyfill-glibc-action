513116aeea09c3901eabd31bf58334a084c922ea
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2024-10-01 16:24:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ fi
 if [ "x$INPUT_TARGET" != "x" ] && [ "x$INPUT_GLIBC" != "x" ]; then
     if [ "x$INPUT_TARGET" != "x" ]; then
         echo "Running polyfill-glibc with target $INPUT_TARGET"
-        name=$(sudo sh -c "polyfill-glibc --target-glibc=$INPUT_GLIBC $INPUT_TARGET || echo $INPUT_TARGET")
+        name=$(sh -c "polyfill-glibc --target-glibc=$INPUT_GLIBC $INPUT_TARGET || echo $INPUT_TARGET")
     fi
     if [ "x$name" == "x" ]; then
         exit 1
